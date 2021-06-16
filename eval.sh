a7a1db7dfe5106f1383cc65e6c02f81bb1648f8d
YuxinZou <zouyux@outlook.com>
2021-06-16 10:37:22 +0800

    delete eval.sh

--- eval.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-set -o pipefail
-
-TASK=$1
-DATASET=$2
-GPU=$3
-
-CUDA_VISIBLE_DEVICES=${GPU} python tools/eval.py -c configs/rec/new_baseline/${TASK}.yml -o Eval.dataset.label_file_list=["./data/train_data/${DATASET}.txt"] Global.pretrained_model="./checkpoint/${TASK}/best_accuracy"
